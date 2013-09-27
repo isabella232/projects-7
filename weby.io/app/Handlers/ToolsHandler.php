fa822953aaff7cf7effd48aa8102c2970c11672a
Pavel Denisjuk <pavel@webiny.com>
2013-09-27 11:45:12 +0200

    Weby.io

--- weby.io/app/Handlers/ToolsHandler.php
+++ weby.io/app/Handlers/ToolsHandler.php
@@ -10,9 +10,9 @@ use App\Lib\Screenshot;
 use App\Lib\Stats;
 use App\Lib\UserTrait;
 use App\Lib\View;
+use Webiny\Component\Cache\CacheTrait;
 use Webiny\Component\Http\HttpTrait;
 use Webiny\Component\Image\ImageTrait;
-use Webiny\Component\Logger\Logger;
 use Webiny\Component\Logger\LoggerTrait;
 use Webiny\Component\StdLib\StdLibTrait;
 use Webiny\Component\Storage\StorageTrait;
@@ -21,232 +21,262 @@ use App\Lib\Logger as WebyLogger;
 
 class ToolsHandler extends AbstractHandler
 {
-    use HttpTrait, LoggerTrait, UserTrait, StorageTrait, StdLibTrait, ImageTrait, AppTrait;
+	use HttpTrait, LoggerTrait, UserTrait, StorageTrait, StdLibTrait, ImageTrait, AppTrait, CacheTrait;
 
-    /**
-     * Log JS exception
-     */
-    public function log()
-    {
-        $errors = json_decode($this->request()->post('errors'), true);
-        $browser = $this->request()->post('browser');
-        foreach ($errors as $e) {
-            $this->logger('webiny_logger')
-                ->warning($e['message'], [
-                    urlencode($e['url']),
-                    $e['line'],
-                    $browser
-                ]);
-            WebyLogger::getInstance()->logError($e['message'], $e['url'], $e['line'], $browser);
-        }
+	/**
+	 * Log JS exception
+	 */
+	public function log() {
+		$errors = json_decode($this->request()->post('errors'), true);
+		$browser = $this->request()->post('browser');
+		foreach ($errors as $e) {
+			$this->logger('webiny_logger')
+			->warning($e['message'], [
+									 urlencode($e['url']),
+									 $e['line'],
+									 $browser
+									 ]);
+			WebyLogger::getInstance()->logError($e['message'], $e['url'], $e['line'], $browser);
+		}
 
-        $this->ajaxResponse(false, 'Got it :)');
-    }
+		$this->ajaxResponse(false, 'Got it :)');
+	}
 
-    public function takeScreenshot($webyId)
-    {
-        if ($this->request()->getClientIp() != '127.0.0.1' && $this->request()->getClientIp() != '192.241.180.184') {
-            $this->request()->redirect($this->app()->getConfig()->app->web_path);
-        }
-        $weby = new WebyEntity();
-        $weby->load($webyId);
+	public function takeScreenshot($webyId) {
+		if($this->request()->getClientIp() != '127.0.0.1' && $this->request()->getClientIp() != '192.241.180.184') {
+			$this->request()->redirect($this->app()->getConfig()->app->web_path);
+		}
+		$weby = new WebyEntity();
+		$weby->load($webyId);
 
-        $screenshot = new Screenshot\Screenshot();
-        $queue = new Screenshot\ScreenshotQueue();
+		$screenshot = new Screenshot\Screenshot();
+		$queue = new Screenshot\ScreenshotQueue();
 
-        $storage = $this->storage('local');
+		$storage = $this->storage('local');
 
-        $key = $weby->getStorageFolder() . '/original-screenshot-' . time() . '.jpg';
-        $path = $storage->getAbsolutePath($key);
-        try {
-            $screenshot->takeScreenshot($weby, $path);
-            $weby->getImage('original-screenshot')->setKey($key)->save();
+		$key = $weby->getStorageFolder() . '/original-screenshot-' . time() . '.jpg';
+		$path = $storage->getAbsolutePath($key);
+		try {
+			$screenshot->takeScreenshot($weby, $path);
+			$weby->getImage('original-screenshot')->setKey($key)->save();
 
-            // Create different image sizes
-            $this->_createSize($weby, $storage, 90, 81, 'dashboard');
-            $this->_createSize($weby, $storage, 215, 180, 'frontend-square');
-            $this->_createSize($weby, $storage, 215, 512, 'frontend-vertical');
-            $this->_createSize($weby, $storage, 515, 180, 'frontend-horizontal');
+			// Create different image sizes
+			$this->_createSize($weby, $storage, 90, 81, 'dashboard');
+			$this->_createSize($weby, $storage, 215, 180, 'frontend-square');
+			$this->_createSize($weby, $storage, 215, 512, 'frontend-vertical');
+			$this->_createSize($weby, $storage, 515, 180, 'frontend-horizontal');
 
-            $queue->complete($webyId)->processQueue();
-        } catch (\Exception $e) {
-            $queue->abort($webyId, $e->getMessage())->processQueue();
-        }
-        die();
-    }
+			system('varnishadm -T 127.0.0.1:6082 -S /etc/varnish/secret ban req.url == "' . $weby->getPublicUrl(true) . '"');
 
-    private function _createSize($weby, $storage, $width, $height, $tag)
-    {
-        $key = $weby->getStorageFolder() . '/' . $tag . '-' . time() . '.jpg';
-        $imageObj = $this->image($weby->getImage('original-screenshot')->getFile());
-        $thumbImage = new LocalFile($key, $storage);
-        if ($imageObj->thumbnail($width, $height, 'crop')->save($thumbImage)) {
-            $weby->getImage($tag)->setKey($key)->save();
-        }
-    }
+			$queue->complete($webyId)->processQueue();
+		} catch (\Exception $e) {
+			$queue->abort($webyId, $e->getMessage())->processQueue();
+		}
+		die();
+	}
 
-    /**
-     * Sends general feedback to email (addresses are in config.yaml)
-     */
-    public function ajaxSendFeedback()
-    {
-        $data = [];
-        $data['name'] = $this->user() ? $this->user()->getFirstName() . ' ' . $this->user()->getLastName() : $this->request('name');
-        $data['email'] = $this->user() ? $this->user()->getEmail() : $this->request('email');
-        $data['message'] = $this->request()->post('message');
+	private function _createSize($weby, $storage, $width, $height, $tag) {
+		$key = $weby->getStorageFolder() . '/' . $tag . '-' . time() . '.jpg';
+		$imageObj = $this->image($weby->getImage('original-screenshot')->getFile());
+		$thumbImage = new LocalFile($key, $storage);
+		if($imageObj->thumbnail($width, $height, 'crop')->save($thumbImage)) {
+			$weby->getImage($tag)->setKey($key)->save();
+		}
+	}
 
-        $config = $this->app()->getConfig();
-        $absPath = $config->app->theme_abs_path;
-        $feedbackReceivers = $config->feedback_receivers->get('general')->toArray();
-        // Send feedback by email
-        $mailer = $this->mailer();
+	/**
+	 * Sends general feedback to email (addresses are in config.yaml)
+	 */
+	public function ajaxSendFeedback() {
+		$data = [];
+		$data['name'] = $this->user() ? $this->user()->getFirstName() . ' ' . $this->user()
+																			  ->getLastName() : $this->request('name');
+		$data['email'] = $this->user() ? $this->user()->getEmail() : $this->request('email');
+		$data['message'] = $this->request()->post('message');
 
-        $mailData = [];
-        foreach ($feedbackReceivers as $receiver) {
-            $mailData[$receiver] = [
-                '{name}' => $data['name'],
-                '{email}' => $data['email'],
-                '{message}' => $data['message']
-            ];
-        }
-        // Let's build our message
-        $msg = $mailer->getMessage();
-        $msg->setSubject('Weby.io - User feedback')
-            ->setBodyFromTemplate($absPath . 'templates/emails/feedback.tpl')
-            ->setContentType('text/html')
-            ->setTo($feedbackReceivers);
+		$config = $this->app()->getConfig();
+		$absPath = $config->app->theme_abs_path;
+		$feedbackReceivers = $config->feedback_receivers->get('general')->toArray();
+		// Send feedback by email
+		$mailer = $this->mailer();
 
-        // Send it
-        $mailer->setDecorators($mailData);
-        $mailer->send($msg);
+		$mailData = [];
+		foreach ($feedbackReceivers as $receiver) {
+			$mailData[$receiver] = [
+				'{name}'    => $data['name'],
+				'{email}'   => $data['email'],
+				'{message}' => $data['message']
+			];
+		}
+		// Let's build our message
+		$msg = $mailer->getMessage();
+		$msg->setSubject('Weby.io - User feedback')
+		->setBodyFromTemplate($absPath . 'templates/emails/feedback.tpl')
+		->setContentType('text/html')
+		->setTo($feedbackReceivers);
 
-        $this->ajaxResponse(false);
-    }
+		// Send it
+		$mailer->setDecorators($mailData);
+		$mailer->send($msg);
 
-    /**
-     * Get user's favorite Webies
-     */
-    public function ajaxGetFavorites()
-    {
-        $favorites = $this->user()->getFavoriteWebies(true);
-        $data = [
-            'favorites' => $this->_truncateWebyTitle(json_decode($favorites, true)),
-            'count' => UserEntity::getTotalRows()
-        ];
-        die($this->request()->query("\$callback") . '(' . json_encode($data) . ')');
-    }
+		$this->ajaxResponse(false);
+	}
 
-    /**
-     * Get user's Webies
-     */
-    public function ajaxGetWebies()
-    {
-        $webies = $this->user()->getWebies(true);
-        $data = [
-            'webies' => $this->_truncateWebyTitle(json_decode($webies, true)),
-            'count' => WebyEntity::getTotalRows()
-        ];
-        die($this->request()->query("\$callback") . '(' . json_encode($data) . ')');
-    }
+	/**
+	 * Get user's favorite Webies
+	 */
+	public function ajaxGetFavorites() {
+		$favorites = $this->user()->getFavoriteWebies(true);
+		$data = [
+			'favorites' => $this->_truncateWebyTitle(json_decode($favorites, true)),
+			'count'     => UserEntity::getTotalRows()
+		];
+		die($this->request()->query("\$callback") . '(' . json_encode($data) . ')');
+	}
+
+	/**
+	 * Get user's Webies
+	 */
+	public function ajaxGetWebies() {
+		$webies = $this->user()->getWebies(true);
+		$data = [
+			'webies' => $this->_truncateWebyTitle(json_decode($webies, true)),
+			'count'  => WebyEntity::getTotalRows()
+		];
+		die($this->request()->query("\$callback") . '(' . json_encode($data) . ')');
+	}
 
 
-    /**
-     * Used to update number of hits of a given Weby
-     */
-    public function ajaxEmbeddedHit($id)
-    {
-        // Firstly, check if we got valid referer
-        $referer = $this->request()->server()->httpReferer();
-        $referer = $referer ? $this->str($referer) : false;
-        if (!$referer || !$referer->startsWith($this->app()->getConfig()->app->web_path)) {
-            die();
-        }
+	/**
+	 * Used to update number of hits of a given Weby
+	 */
+	public function ajaxEmbeddedHit($id) {
+		// Firstly, check if we got valid referer
+		$referer = $this->request()->server()->httpReferer();
+		$referer = $referer ? $this->str($referer) : false;
+		if(!$referer || !$referer->startsWith($this->app()->getConfig()->app->web_path)) {
+			die();
+		}
 
-        // If everything went okay, then check if we got valid Weby
-        $weby = new WebyEntity();
-        if (!$weby->load($id)) {
-            $this->ajaxResponse(true, 'Could not find Weby!');
-        }
+		// If everything went okay, then check if we got valid Weby
+		$weby = new WebyEntity();
+		if(!$weby->load($id)) {
+			$this->ajaxResponse(true, 'Could not find Weby!');
+		}
 
-        // Finally, update hit stats
-        Stats\Stats::getInstance()->updateWebyEmbeddedHits($weby);
-        die();
-    }
+		// Finally, update hit stats
+		Stats\Stats::getInstance()->updateWebyEmbeddedHits($weby);
+		die();
+	}
 
-    /**
-     * View Weby for screnshot
-     * @param $id
-     */
-    public function viewWeby($id)
-    {
-        $weby = new WebyEntity();
-        $this->weby = $weby->load($id);
-        $this->setTemplatePath('templates/pages')->setTemplate('screenshot');
-    }
+	/**
+	 * View Weby for screnshot
+	 *
+	 * @param $id
+	 */
+	public function viewWeby($id) {
+		$weby = new WebyEntity();
+		$this->weby = $weby->load($id);
+		$this->setTemplatePath('templates/pages')->setTemplate('screenshot');
+	}
 
-    /**
-     * Mark Weby deleted
-     * @param $webyId
-     */
-    public function deleteWeby($webyId)
-    {
-        $weby = new WebyEntity();
-        $weby->load($webyId);
+	/**
+	 * Mark Weby deleted
+	 *
+	 * @param $webyId
+	 */
+	public function deleteWeby($webyId) {
+		$weby = new WebyEntity();
+		$weby->load($webyId);
 
-        if ($weby->getUser()->getId() != $this->user()->getId()) {
-            $this->ajaxResponse(true, 'You can not delete a Weby that does not belong to you!');
-        }
+		if($weby->getUser()->getId() != $this->user()->getId()) {
+			$this->ajaxResponse(true, 'You can not delete a Weby that does not belong to you!');
+		}
 
-        $weby->delete();
-        $this->ajaxResponse(false);
-    }
+		$weby->delete();
+		$this->ajaxResponse(false);
+	}
 
-    public function ajaxSearchTags()
-    {
-        $search = $this->request()->post('search');
-        $this->_sanitizeInput($search, true);
-        $result = WebyEntity::searchTags($search, true, true);
-        header('Content-type: application/json; charset=utf-8;');
-        die($result);
-    }
+	public function ajaxSearchTags() {
+		$search = $this->request()->post('search');
+		$this->_sanitizeInput($search, true);
+		$result = WebyEntity::searchTags($search, true, true);
+		header('Content-type: application/json; charset=utf-8;');
+		die($result);
+	}
 
-    // TODO: this isn't finished as some parts are still needed to complete this (image and tags)
-    /**
-     * Gets Webies from given tags
-     * Also used by Wordpress Webies widget
-     */
-    public function ajaxGetWebiesByTags()
-    {
-        $tags = $this->request()->query('q');
-        $webiesIds = WebyEntity::getWebiesByTags($tags);
-        $json = [];
-        $weby = new WebyEntity();
-        foreach ($webiesIds as $id) {
-            $weby->load($id);
-            $temp['id'] = $weby->getId();
-            $temp['title'] = $weby->getTitle();
-            $temp['thumbnail'] = 'http://www.hdwallpaperstop.com/wp-content/uploads/2013/02/Cute-Puppy-Black-Eyes-Wallpaper.jpg';
-            $temp['author'] = $weby->getUser()->getFirstName() . ' ' . $weby->getUser()->getLastName();
-            $temp['hits'] = $weby->getTotalHits();
-            $temp['favorites'] = $weby->getFavoriteCount();
-            $temp['url'] = $weby->getPublicUrl();
-            $json[] = $temp;
-        }
+	public function mustacheWeby($webyId) {
 
-        header('Content-type: application/json; charset=utf-8;');
-        die("showWebies(" . json_encode($json) . ");");
-    }
+		$data = $this->cache()->read('weby.json.' . $webyId);
 
-    private function _truncateWebyTitle($webies)
-    {
-        foreach ($webies as &$w) {
-            $title = $this->str($w['title']);
-            if ($title->length() > 35) {
-                $w['title'] = $title->truncate(32, '...')->val();
-            }
-        }
+		$weby = new WebyEntity();
+		$weby->load($webyId);
 
-        return $webies;
-    }
+		if(!$data) {
+			$data = [
+				'favorite_count' => $weby->getFavoriteCount(),
+				'favorited_by'   => $weby->getUsersFavorited(true),
+				'hits'           => $weby->getTotalHits(),
+				'webyUser'       => [
+					'id'        => $weby->getUser()->getId(),
+					'name'      => $weby->getUser()->getUsername(),
+					'avatar'    => $weby->getUser()->getAvatarUrl(),
+					'followers' => $weby->getUser()->getFollowingUsersCount()
+				],
+				'tags'           => $weby->getTags(true),
+				'is_favorited'   => $weby->inUsersFavorites(),
+				'share_count'    => $weby->getShareCount()
+			];
+
+			$this->cache()->save('weby.json.' . $webyId, json_encode($data));
+		} else {
+			$data = json_decode($data, true);
+		}
+
+		$data['currentUser'] = [
+			'name'         => $this->user()->getFirstName() . ' ' . $this->user()->getLastName()[0] . '.',
+			'avatar'       => $this->user()->getAvatarUrl(),
+			'is_following' => $this->user()->isFollowing($weby->getUser())
+		];
+
+		die(json_encode($data));
+	}
+
+	// TODO: this isn't finished as some parts are still needed to complete this (image and tags)
+	/**
+	 * Gets Webies from given tags
+	 * Also used by Wordpress Webies widget
+	 */
+	public function ajaxGetWebiesByTags() {
+		$tags = $this->request()->query('q');
+		$webiesIds = WebyEntity::getWebiesByTags($tags);
+		$json = [];
+		$weby = new WebyEntity();
+		foreach ($webiesIds as $id) {
+			$weby->load($id);
+			$temp['id'] = $weby->getId();
+			$temp['title'] = $weby->getTitle();
+			$temp['thumbnail'] = 'http://www.hdwallpaperstop.com/wp-content/uploads/2013/02/Cute-Puppy-Black-Eyes-Wallpaper.jpg';
+			$temp['author'] = $weby->getUser()->getFirstName() . ' ' . $weby->getUser()->getLastName();
+			$temp['hits'] = $weby->getTotalHits();
+			$temp['favorites'] = $weby->getFavoriteCount();
+			$temp['url'] = $weby->getPublicUrl();
+			$json[] = $temp;
+		}
+
+		header('Content-type: application/json; charset=utf-8;');
+		die("showWebies(" . json_encode($json) . ");");
+	}
+
+	private function _truncateWebyTitle($webies) {
+		foreach ($webies as &$w) {
+			$title = $this->str($w['title']);
+			if($title->length() > 35) {
+				$w['title'] = $title->truncate(32, '...')->val();
+			}
+		}
+
+		return $webies;
+	}
 
 }
